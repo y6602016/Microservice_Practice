481cff5229ddfdcf5cc10aa77d9bbe214467a0bb
mike <y6602016@gmail.com>
2021-07-03 15:40:06 +0800

    integrate kubernetes

--- posts/index.js
+++ posts/index.js
@@ -21,7 +21,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: { id, title },
   });
@@ -35,5 +35,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log("v25");
   console.log("Listening on 4000");
 });
